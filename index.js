c761aa119b86bd41243e6c5f7ab5ca971c5b5984
JClark <justindanielclark@gmail.com>
2022-07-17 22:44:16 -0700

    Dynamically Adding Flex Items Now, added click events to remove

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const flexContent = Array.from(document.querySelectorAll('.item'));
+const flexContainer = document.querySelector('.flex-container');
+const items = [];
 
-window.addEventListener('DOMContentLoaded', updateContentInnerHTML)
-window.addEventListener('resize', updateContentInnerHTML);
 
-function updateContentInnerHTML(e){
-    flexContent.forEach(element=>{
-        let box = element.getBoundingClientRect();
-        element.children[0].innerHTML = `Width: ${box.width}, Height: ${box.height}`;
+window.addEventListener('load', ()=> {
+    for(let i = 0; i < 3; i++){
+        flexContainer.appendChild(createFlexItem(items));
+    }
+});
+
+function createFlexItem(items){
+    const item = document.createElement('div');
+    item.classList.toggle('item');
+    item.style.backgroundColor = createColor();
+
+    const cancel = document.createElement('div');
+    cancel.classList.toggle('material-symbols-outlined');
+    cancel.innerHTML = 'cancel';
+    cancel.addEventListener('click', ()=>{
+        flexContainer.removeChild(item);
     })
+    item.appendChild(cancel);
+
+    const id = document.createElement('div');
+    id.classList.toggle('item-id');
+    id.innerHTML = items.length;
+    item.appendChild(id);
+
+    const content = document.createElement('div');
+    content.classList.toggle('flex-content');
+    content.innerHTML = 'Content';
+    item.appendChild(content);
+
+    items.push(item);
+
+    return item;
+}
+
+function createColor(){
+    return `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`;
 }
