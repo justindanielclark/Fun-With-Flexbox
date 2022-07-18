f8dd720dc8bcfb2d9e71584e36b84c79f3fa63e2
JClark <justindanielclark@gmail.com>
2022-07-18 00:23:05 -0700

    Functionality for Controls Added

--- index.js
+++ index.js
@@ -1,14 +1,99 @@
 const flexContainer = document.querySelector('.flex-container');
-const items = [];
+const startingItems = 3;
+let itemCount = {count: 0};
 
 
 window.addEventListener('load', ()=> {
-    for(let i = 0; i < 3; i++){
-        flexContainer.appendChild(createFlexItem(items));
+    for(let i = 0; i < startingItems; i++){
+        createFlexItem(itemCount);
     }
 });
 
-function createFlexItem(items){
+document.querySelector('button').addEventListener('click', ()=>{
+    createFlexItem(itemCount);
+})
+
+//Flex-Direction
+document.querySelector('#flex-direction').addEventListener('change', (e)=>{
+    flexContainer.style.flexDirection = e.target.value;
+})
+//Flex-Wrap
+document.querySelector('#flex-wrap').addEventListener('change', (e)=>{
+    flexContainer.style.flexWrap = e.target.value;
+})
+//Justify-Content
+document.querySelector('#justify-content').addEventListener('change', (e)=>{
+    flexContainer.style.justifyContent = e.target.value;
+})
+//Align-Items
+document.querySelector('#align-items').addEventListener('change', (e)=>{
+    flexContainer.style.alignItems = e.target.value;
+})
+//Align-Content
+document.querySelector('#align-content').addEventListener('change', (e)=>{
+    flexContainer.style.alignContent = e.target.value;
+})
+//Flex-Grow
+document.querySelector('#flex-grow').addEventListener('change', (e)=>{
+    if(e.target.value < 0) {
+      e.target.value = 0;
+      return;  
+    }
+    Array.from(document.querySelectorAll('.item')).forEach(item=>{
+        e.target.value = Math.floor(e.target.value);
+        item.style.flexGrow = Math.floor(e.target.value);
+    })
+})
+
+//Flex-Shrink
+document.querySelector('#flex-shrink').addEventListener('change', (e)=>{
+    if(e.target.value < 0) {
+      e.target.value = 0;
+      return;  
+    }
+    Array.from(document.querySelectorAll('.item')).forEach(item=>{
+        e.target.value = Math.floor(e.target.value);
+        item.style.flexShrink = Math.floor(e.target.value);
+    })
+})
+
+//Flex-Basis
+document.querySelector('#flex-basis').addEventListener('change', (e)=>{
+    if(e.target.value < 0) {
+      e.target.value = 0;
+      return;  
+    }
+    Array.from(document.querySelectorAll('.item')).forEach(item=>{
+        e.target.value = Math.floor(e.target.value);
+        item.style.flexBasis = `${Math.floor(e.target.value)}px`;
+    })
+})
+
+//Min-Height
+document.querySelector('#min-height').addEventListener('change', (e)=>{
+    if(e.target.value < 0) {
+      e.target.value = 0;
+      return;  
+    }
+    Array.from(document.querySelectorAll('.item')).forEach(item=>{
+        e.target.value = Math.floor(e.target.value);
+        item.style.minHeight = `${Math.floor(e.target.value)}px`;
+    })
+})
+
+//Min-Width
+document.querySelector('#min-width').addEventListener('change', (e)=>{
+    if(e.target.value < 0) {
+      e.target.value = 0;
+      return;  
+    }
+    Array.from(document.querySelectorAll('.item')).forEach(item=>{
+        e.target.value = Math.floor(e.target.value);
+        item.style.minWidth = `${Math.floor(e.target.value)}px`;
+    })
+})
+
+function createFlexItem(itemCount){
     const item = document.createElement('div');
     item.classList.toggle('item');
     item.style.backgroundColor = createColor();
@@ -23,17 +108,23 @@ function createFlexItem(items){
 
     const id = document.createElement('div');
     id.classList.toggle('item-id');
-    id.innerHTML = items.length;
+    id.innerHTML = itemCount.count;
+    itemCount.count = itemCount.count + 1;
     item.appendChild(id);
 
-    const content = document.createElement('div');
-    content.classList.toggle('flex-content');
-    content.innerHTML = 'Content';
-    item.appendChild(content);
+    // const content = document.createElement('div');
+    // content.classList.toggle('flex-content');
+    //     const contentWidth = document.createElement('p');
+    //     contentWidth.classList.toggle('content-info');
+    //     contentWidth.innerHTML = 'Width: ___'
+    //     content.appendChild(contentWidth);
+    //     const contentHeight = document.createElement('p');
+    //     contentHeight.classList.toggle('content-info');
+    //     contentHeight.innerHTML = 'Height: ___'
+    //     content.appendChild(contentHeight);
+    // item.appendChild(content);
 
-    items.push(item);
-
-    return item;
+    flexContainer.appendChild(item);
 }
 
 function createColor(){
